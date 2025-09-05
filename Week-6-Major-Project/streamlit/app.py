c5f3f015b0fd1142dd66bb4e3a6648d06e8e6ff6
sahiladlakha33 <158745806+sahiladlakha33@users.noreply.github.com>
2025-09-05 18:15:53 +0530

    Update

--- Week-6-Major-Project/streamlit/app.py
+++ Week-6-Major-Project/streamlit/app.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import StandardScaler
 # Load trained model
 @st.cache_resource
 def load_model():
-    return joblib.load("../Model/credit_card_fraud_detection_model.pkl")
+    return joblib.load("credit_card_fraud_detection_model.pkl")
 
 model = load_model()
 
